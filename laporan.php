0ddd481c6e73640f10d9e9f58e10e9e5950a445a
ismiputri37 <ismi.putri397@gmail.com>
2025-01-27 13:55:21 +0800

    laporan peminjaman done

---
+++ laporan.php
@@ -0,0 +1,41 @@
+<h1 class="mt-4">Kategori Buku</h1>
+<div class="row">
+    <div class="col-md-12">
+        <a href="cetak.php" target="blank" class="btn btn-primary">Cetak Data</a>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <tr>
+                <th>No.</th>
+                <th>User</th>
+                <th>Buku</th>
+                <th>Tanggal Peminjaman</th>
+                <th>Tanggal Pengembalian</th>
+                <th>Status Peminjaman</th>
+            </tr>
+            
+            <?php 
+                $i=1;
+                $query = mysqli_query($koneksi, "SELECT * FROM peminjaman LEFT JOIN user on user.id_user=peminjaman.id_user LEFT JOIN buku ON buku.id_buku=peminjaman.id_buku");
+                while ($data = mysqli_fetch_array($query)) :
+            ?>
+            <tr>
+                <td><?= $i++; ?></td>
+                <!-- nama user -->
+                <td><?= $data['nama']; ?></td> 
+                <td><?= $data['judul']; ?></td> 
+                <td><?= $data['tanggal_peminjaman']; ?></td> 
+                <td><?= $data['tanggal_pengembalian']; ?></td> 
+                <td><?= $data['status_peminjaman']; ?></td> 
+
+
+                <td>
+                    <!-- update kategori by id_kategori -->
+                    <a href="?page=ulasan_ubah&&id=<?= $data['id_ulasan']?>" class="btn btn-info">Ubah</a>
+                    <a onclick="return confirm('Apakah anda yakin menghapus data ini')" href="?page=ulasan_hapus&&id=<?= $data['id_ulasan']?>" class="btn btn-danger">Hapus</a>
+                </td>
+            </tr>
+            
+            <?php endwhile; ?>            
+            
+        </table>
+    </div>
+</div>

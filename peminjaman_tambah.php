403aa8df10a4da6cd2babe4e9f03d0a174ae383f
ismiputri37 <ismi.putri397@gmail.com>
2025-02-05 00:14:36 +0800

    gagal store data into db

--- peminjaman_tambah.php
+++ peminjaman_tambah.php
@@ -4,27 +4,50 @@
         <div class="row">
             <div class="col-md-12">
                 <!-- menyimpan data ke database -->
-                <form action="" method="POST">
+                <form action="?page=laporan" method="POST">
                     <?php 
                     // $id = $_GET['id'];
+                    // $maxPinjam = 2;
                         if(isset($_POST['submit'])) {
                             $id_buku = $_POST["id_buku"];
-                            $id_user = $_SESSION['user']['id_user'];
+                            $id_user = $_POST['id_user'];
                             $tanggal_peminjaman = $_POST['tanggal_peminjaman'];
                             $tanggal_pengembalian = $_POST['tanggal_pengembalian'];
+                            // $tanggal_jatuh_tempo = $_POST['tanggal_jatuh_tempo'];
                             $status_peminjaman = $_POST['status_peminjaman'];
-                            $query = mysqli_query($koneksi, "INSERT INTO peminjaman(id_buku, id_user, tanggal_peminjaman, tanggal_pengembalian, status_peminjaman) 
-                                VALUES('$id_buku', '$id_user', '$tanggal_peminjaman', '$tanggal_pengembalian', '$status_peminjaman')");                            
-                            if($query) {
-                                echo '<script>alert("Tambah data berhasil"); </script>';
+
+                            $cek = mysqli_query($koneksi, "SELECT * FROM peminjaman WHERE id_user='$id_user' AND status_peminjaman='$status_peminjaman'");
+                            $check = mysqli_num_rows($cek);
+                            if ($check >= 2) {
+                                echo "User sudah melebihi maksimal peminjaman";
                             } else {
-                                echo '<script> alert("Tambah data gagal");</script>';
-                            }
-                        }
-                        
-                    ?>
+                                $query = mysqli_query($koneksi, "INSERT INTO peminjaman(id_buku, id_user, tanggal_peminjaman, tanggal_pengembalian, status_peminjaman) 
+                                VALUES('$id_buku', '$id_user', '$tanggal_peminjaman', '$tanggal_pengembalian', '$status_peminjaman')");                            
+                                if($query) {
+                                    echo '<script> alert("Tambah data berhasil"); </script>';
+                                } else {
+                                    echo '<script> alert("Tambah data gagal"); </script>';
+                                }
+                            }                            
+                        }                        
+                    ?>s
                     <!-- menampilkan nama kategori -->
                     <div class="row">
+                        <div class="row mb-3">
+                            <div class="col-md-2">User</div>
+                            <div class="col-md-8">
+                                <select name="id_user" class="form-control">
+                                    <?php 
+                                        $pemin = mysqli_query($koneksi, "SELECT * FROM user");
+                                        while ($peminjam = mysqli_fetch_array($pemin)) :
+                                    ?>
+                                        <option value="<?= $peminjam['id_user']; ?>">
+                                            <?= $peminjam['nama']; ?>
+                                        </option>
+                                    <?php endwhile; ?>
+                                </select>
+                            </div>
+                        </div>
                         <div class="row mb-3">
                             <div class="col-md-2">Buku</div>
                             <div class="col-md-8">
@@ -45,7 +68,7 @@
                         <div class="row mb-3">
                             <div class="col-md-2">Tanggal Peminjaman</div>
                             <div class="col-md-8">
-                                <input type="date" class="form-control" name="tanggal_peminjaman">
+                                <input type="date" class="form-control" name="tanggal_peminjaman" required>
                             </div>
                         </div>
                         <div class="row mb-3">
@@ -54,6 +77,12 @@
                                 <input type="date" class="form-control" name="tanggal_pengembalian">
                             </div>
                         </div>
+                        <!-- <div class="row mb-3">
+                            <div class="col-md-2">Tanggal Jatuh Tempo</div>
+                            <div class="col-md-8">
+                                <input type="date" class="form-control" name="tanggal_jatuh_tempo">
+                            </div>
+                        </div> -->
                         <div class="row mb-3">
                             <div class="col-md-2">Status Peminjaman</div>
                             <div class="col-md-8">
@@ -70,7 +99,7 @@
                                 <div class="col-md-8">
                                     <button type="submit" class="btn btn-primary" name="submit" value="submit">Simpan</button>
                                     <button type="reset" class="btn btn-secondary">Reset</button>
-                                    <a href="?page=peminjaman" class="btn btn-danger">Kembali</a>
+                                    <a href="?page=laporan" class="btn btn-danger">Kembali</a>
                                 </div>
                             </div>
                         </div>

1619b16b28283a05a2a16bdffb0b25ce71ae1149
ismiputri37 <ismi.putri397@gmail.com>
2025-02-05 00:19:29 +0800

    add data buku done

--- buku_tambah.php
+++ buku_tambah.php
@@ -1,4 +1,4 @@
-<h1 class="mt-4">Buku</h1>
+<h1 class="mt-4">Tambah Buku</h1>
 <div class="card">
     <div class="card-body">
         <div class="row">
@@ -9,19 +9,25 @@
                         if(isset($_POST['submit'])) {
                             // $buku = $_POST['buku'];
                             $id_kategori = $_POST['id_kategori'];
-                            $judul = $_POST['judul'];
-                            $penulis = $_POST['penulis'];
-                            $penerbit = $_POST['penerbit'];
+                            $judul = ucfirst($_POST['judul']);
+                            $penulis = ucfirst($_POST['penulis']);
+                            $penerbit = ucfirst($_POST['penerbit']);
                             $tahun_terbit = $_POST['tahun_terbit'];
+                            $kuantitas = $_POST['kuantitas'];
                             $deskripsi = $_POST['deskripsi'];
-
-                            $query = mysqli_query($koneksi, "INSERT INTO buku(id_kategori, judul, penulis, penerbit, tahun_terbit, deskripsi) 
-                                VALUES('$id_kategori', '$judul', '$penulis', '$penerbit', '$tahun_terbit', '$deskripsi')");
-                            if($query) {
-                                echo '<script>alert("Tambah data berhasil"); </script>';
+                            $cek = mysqli_query($koneksi, "SELECT * FROM buku WHERE UPPER(judul)='$judul'");
+                            $check = mysqli_num_rows($cek);
+                            if ($check > 0) {
+                                echo "Data pernah dimasukkan";
                             } else {
-                                echo '<script> alert("Tambah data gagal");</script>';
-                            }
+                                $query = mysqli_query($koneksi, "INSERT INTO buku(id_kategori, judul, penulis, penerbit, tahun_terbit, kuantitas, deskripsi) 
+                                VALUES('$id_kategori', '$judul', '$penulis', '$penerbit', '$tahun_terbit', '$kuantitas','$deskripsi')");
+                                if($query) {
+                                    echo '<script>alert("Tambah data berhasil"); </script>';
+                                } else {
+                                    echo '<script> alert("Tambah data gagal");</script>';
+                                }
+                            }                           
                         }
                     ?>
                     <!-- menampilkan nama kategori -->
@@ -45,23 +51,27 @@
                         <!-- menampilkan judul buku -->
                         <div class="row mb-3">
                             <div class="col-md-2">Judul</div>
-                            <div class="col-md-8"><input type="text" class="form-control" name="judul"></div>
+                            <div class="col-md-8"><input type="text" class="form-control" name="judul" required></div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-2">Penulis</div>
-                            <div class="col-md-8"><input type="text" class="form-control" name="penulis"></div>
+                            <div class="col-md-8"><input type="text" class="form-control" name="penulis" required></div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-2">Penerbit</div>
-                            <div class="col-md-8"><input type="text" class="form-control" name="penerbit"></div>
+                            <div class="col-md-8"><input type="text" class="form-control" name="penerbit" required></div>
                         </div><div class="row mb-3">
                             <div class="col-md-2">Tahun Terbit</div>
-                            <div class="col-md-8"><input type="number" class="form-control" name="tahun_terbit" min="1900" max="2025"></div>
+                            <div class="col-md-8"><input type="number" class="form-control" name="tahun_terbit" min="1900" max="2025" required></div>
+                        </div>
+                        </div><div class="row mb-3">
+                            <div class="col-md-2">Jumlah</div>
+                            <div class="col-md-8"><input type="number" class="form-control" name="kuantitas" required></div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-2">Deskripsi</div>
                             <div class="col-md-8">
-                                <textarea name="deskripsi" rows="5" class="form-control"></textarea>
+                                <textarea name="deskripsi" rows="5" class="form-control" required></textarea>
                             </div>
                         </div>
 
@@ -72,7 +82,7 @@
                                 <div class="col-md-8">
                                     <button type="submit" class="btn btn-primary" name="submit" value="submit">Simpan</button>
                                     <button type="reset" class="btn btn-secondary">Reset</button>
-                                    <a href="?page=kategori" class="btn btn-danger">Kembali</a>
+                                    <a href="?page=buku" class="btn btn-danger">Kembali</a>
                                 </div>
                             </div>
                         </div>

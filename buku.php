e75e5bcbfaac7e0acf408615eccd6449501367eb
ismiputri37 <ismi.putri397@gmail.com>
2025-02-05 00:18:50 +0800

    add data buku done

--- buku.php
+++ buku.php
@@ -1,7 +1,11 @@
 <h1 class="mt-4">Kategori Buku</h1>
 <div class="row">
     <div class="col-md-12">
-        <a href="?page=buku_tambah" class="btn btn-primary">Tambah Data</a>
+        <?php 
+        if ($_SESSION['user']['level'] !='peminjam') {
+        ?>
+            <a href="?page=buku_tambah" class="btn btn-primary">Tambah Data</a>
+        <?php } ?>
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <tr>
                 <th>No.</th>
@@ -10,8 +14,13 @@
                 <th>Penulis</th>
                 <th>Penerbit</th>
                 <th>Tahun Terbit</th>
+                <th>Jumlah</th>
                 <th>Deskripsi</th>
+                <?php 
+                if ($_SESSION['user']['level'] !='peminjam') :
+                ?>
                 <th>Aksi</th>
+                <?php endif; ?>
             </tr>
             <?php 
                 $i=1;
@@ -25,11 +34,16 @@
                 <td><?= $data['penulis']; ?></td>
                 <td><?= $data['penerbit']; ?></td>
                 <td><?= $data['tahun_terbit']; ?></td>
+                <td><?= $data['kuantitas']; ?></td>
                 <td><?= $data['deskripsi']; ?></td>
+                <?php 
+                if ($_SESSION['user']['level'] !='peminjam') :
+                ?>
                 <td>
                     <a href="?page=buku_ubah&&id=<?= $data['id_buku']?>" class="btn btn-info">Ubah</a>
                     <a onclick="return confirm('Apakah anda yakin menghapus data ini')" href="?page=buku_hapus&&id=<?= $data['id_buku']?>" class="btn btn-danger">Hapus</a>
                 </td>
+                <?php endif; ?>
             </tr>
 
             <?php endwhile; ?>            

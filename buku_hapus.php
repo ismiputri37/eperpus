27f1a95ecb3144f596a0cf68eecbec49be142f1f
chud-lori <imlori000@gmail.com>
2025-02-11 16:46:54 +0700

    remove uploaded image when hapus buku

--- buku_hapus.php
+++ buku_hapus.php
@@ -1,6 +1,18 @@
-<?php 
+<?php
 $id = $_GET['id'];
-$query = mysqli_query($koneksi, "DELETE FROM buku WHERE id_buku=$id")
+
+// Retrieve the image filename from the database
+$query = mysqli_query($koneksi, "SELECT gambar FROM buku WHERE id_buku=$id");
+$data = mysqli_fetch_array($query);
+$gambar = $data['gambar'];
+
+// Delete the book record from the database
+$query = mysqli_query($koneksi, "DELETE FROM buku WHERE id_buku=$id");
+
+// Delete the image file from the upload directory
+if ($gambar && file_exists("upload/" . $gambar)) {
+    unlink("upload/" . $gambar);
+}
 ?>
 <script>
     alert('Hapus data berhasil');

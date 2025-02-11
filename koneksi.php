39c1c0e8a0ba29555e3272dbff7cf88d07f7ab23
chud-lori <imlori000@gmail.com>
2025-02-11 20:06:51 +0700

    fix koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,12 @@
-<?php 
-session_start();
-$koneksi = mysqli_connect("localhost", "root", "", "pustaka");
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$koneksi = mysqli_connect("localhost", "root", "", "eperpus");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>

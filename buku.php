04f0f7f64641ec68999bf910e8f8b4c6e6673ab9
chud-lori <imlori000@gmail.com>
2025-02-11 16:25:37 +0700

    add search in buku.php

--- buku.php
+++ buku.php
@@ -1,11 +1,31 @@
 <h1 class="mt-4">Daftar Buku</h1>
 <div class="row">
     <div class="col-md-12">
-        <?php 
-        if ($_SESSION['user']['level'] !='peminjam') :
+        <?php
+        if ($_SESSION['user']['level'] != 'peminjam') :
         ?>
             <a href="?page=buku_tambah" class="btn btn-primary">Tambah Data</a>
-        <?php endif;?>
+        <?php endif; ?>
+
+        <!-- Search form -->
+        <form method="GET" action="">
+            <input type="hidden" name="page" value="buku">
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <input type="text" name="nama" class="form-control" placeholder="Nama Buku" value="<?= isset($_GET['nama']) ? $_GET['nama'] : '' ?>">
+                </div>
+                <div class="col-md-3">
+                    <input type="text" name="tahun_terbit" class="form-control" placeholder="Tahun Terbit" value="<?= isset($_GET['tahun_terbit']) ? $_GET['tahun_terbit'] : '' ?>">
+                </div>
+                <div class="col-md-3">
+                    <input type="text" name="penulis" class="form-control" placeholder="Penulis" value="<?= isset($_GET['penulis']) ? $_GET['penulis'] : '' ?>">
+                </div>
+                <div class="col-md-3">
+                    <button type="submit" class="btn btn-primary">Cari</button>
+                </div>
+            </div>
+        </form>
+
         <!-- tabel list buku -->
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <tr>
@@ -17,18 +37,34 @@
                 <th>Penerbit</th>
                 <th>Tahun Terbit</th>
                 <th>ISBN</th>
-                <th>Jumlah</th>                
+                <th>Jumlah</th>
                 <th>Sinopsis</th>
-                <?php 
-                if ($_SESSION['user']['level'] !='peminjam') :
+                <?php
+                if ($_SESSION['user']['level'] == 'admin') :
                 ?>
                 <th>Aksi</th>
                 <?php endif; ?>
             </tr>
-            <?php 
-                $i=1;
-                $query = mysqli_query($koneksi, "SELECT * FROM buku LEFT JOIN kategori ON buku.id_kategori = kategori.id_kategori");
-                while ($data = mysqli_fetch_array($query)) :
+            <?php
+                $i = 1;
+                $nama = isset($_GET['nama']) ? $_GET['nama'] : '';
+                $tahun_terbit = isset($_GET['tahun_terbit']) ? $_GET['tahun_terbit'] : '';
+                $penulis = isset($_GET['penulis']) ? $_GET['penulis'] : '';
+
+                $query = "SELECT buku.*, kategori.kategori FROM buku JOIN kategori ON buku.id_kategori = kategori.id_kategori WHERE 1=1";
+
+                if ($nama != '') {
+                    $query .= " AND buku.judul LIKE '%$nama%'";
+                }
+                if ($tahun_terbit != '') {
+                    $query .= " AND buku.tahun_terbit LIKE '%$tahun_terbit%'";
+                }
+                if ($penulis != '') {
+                    $query .= " AND buku.penulis LIKE '%$penulis%'";
+                }
+
+                $result = mysqli_query($koneksi, $query);
+                while ($data = mysqli_fetch_array($result)) :
             ?>
             <tr>
                 <td><?= $i++; ?></td>
@@ -41,18 +77,16 @@
                 <td><?= $data['isbn']; ?></td>
                 <td><?= $data['jumlah']; ?></td>
                 <td><?= $data['sinopsis']; ?></td>
-                <?php 
-                if ($_SESSION['user']['level'] !='peminjam') :
+                <?php
+                if ($_SESSION['user']['level'] != 'peminjam') :
                 ?>
                 <td>
-                    <a href="?page=buku_ubah&&id=<?= $data['id_buku']?>" class="btn btn-info">Ubah</a>
-                    <a onclick="return confirm('Apakah anda yakin menghapus data ini')" href="?page=buku_hapus&&id=<?= $data['id_buku']?>" class="btn btn-danger">Hapus</a>
+                    <a href="?page=buku_ubah&&id=<?= $data['id_buku'] ?>" class="btn btn-info">Ubah</a>
+                    <a onclick="return confirm('Apakah anda yakin menghapus data ini')" href="?page=buku_hapus&&id=<?= $data['id_buku'] ?>" class="btn btn-danger">Hapus</a>
                 </td>
                 <?php endif; ?>
             </tr>
-
-            <?php endwhile; ?>            
-            
+            <?php endwhile; ?>
         </table>
     </div>
 </div>
